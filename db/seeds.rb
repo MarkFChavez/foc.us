280c6987a573594cc1b78327868a354023e1a1f4
Mark Chavez <markchav3z@gmail.com>
2018-01-22 15:30:49 +0800

    Change primary colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,11 @@
 
 @user.notes.create!(description: "I love this note!")
 @user.notes.create!(description: "NBA Live")
+@user.notes.create!(description: "Indian Summer Travel Video")
+@user.notes.create!(description: "Prep for tomorrow!")
+@user.notes.create!(description: "Finish this Rails app!")
+@user.notes.create!(description: "In addition to the standard responsive variations, text style utilties also come in hover: variations that apply the given text style on hover.")
+@user.notes.create!(description: "If you don't plan to use the text style utilities in your project, you can disable them entirely by setting the textStyle property to false in the modules section of your config file:")
 
 todo1 = @user.todos.create!(
   description: "Research about blockchain"
