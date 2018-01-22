3d5354631e7a74ee8c40d4d04e29670ee308ea45
Mark Chavez <markchav3z@gmail.com>
2018-01-22 19:55:44 +0800

    Generate devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  layout :set_layout
+
+  def set_layout
+    user_signed_in? ? "application" : "not_authenticated"
+  end
 end
