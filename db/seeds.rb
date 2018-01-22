704e6ad677e812dc44c9736b1068a7f37f6fc348
Mark Chavez <markchav3z@gmail.com>
2018-01-22 11:37:30 +0800

    basic dashboard design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@user = User.new(email: "mjfchavez@gmail.com")
+@user.password = "foobar123"
+@user.save!
+
+@user.notes.create!(description: "I love this note!")
+@user.notes.create!(description: "NBA Live")
+
+todo1 = @user.todos.create!(
+  description: "Research about blockchain"
+)
+todo2 = @user.todos.create!(
+  description: "Research about remittances in the Philippines"
+)
+
+todo1.action_items.create!(
+  description: "What is Bitcoin?",
+  done: false
+)
+
+todo1.action_items.create!(
+  description: "What is Ethereum?",
+  done: false
+)
+
+todo2.action_items.create!(
+  description: "How it works?",
+  done: false
+)
