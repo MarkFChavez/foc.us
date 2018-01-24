55438b7df2176ad136c0682688cd559c5e66a284
Mark Chavez <markchav3z@gmail.com>
2018-01-24 15:24:25 +0800

    use if

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,27 +4,27 @@ tech = Category.create(title: "tech")
 learning = Category.create(title: "learning")
 other = Category.create(title: "other")
 
-return unless Rails.env.development?
+if Rails.env.development?
+  mark = User.new(email: "mjfchavez@gmail.com")
+  mark.password = "foobar123"
+  mark.save!
 
-mark = User.new(email: "mjfchavez@gmail.com")
-mark.password = "foobar123"
-mark.save!
-
-note = mark.notes.create(
-  description: %Q(
-    The Ideal Team Player: How to Recognize and
-    Cultivate the Three Essential Virtues: A Leadership
-    Fable
+  note = mark.notes.create(
+    description: %Q(
+      The Ideal Team Player: How to Recognize and
+      Cultivate the Three Essential Virtues: A Leadership
+      Fable
+    )
   )
-)
-note.item_categories.create(category: personal)
+  note.item_categories.create(category: personal)
 
-note = mark.notes.create(
-  description: %Q(
-    Blockchain is a network of computers having an identical
-    copy of the database and changing its state/records by a
-    common agreement based on pure mathematics.
+  note = mark.notes.create(
+    description: %Q(
+      Blockchain is a network of computers having an identical
+      copy of the database and changing its state/records by a
+      common agreement based on pure mathematics.
+    )
   )
-)
-note.item_categories.create(category: work)
-note.item_categories.create(category: personal)
+  note.item_categories.create(category: work)
+  note.item_categories.create(category: personal)
+end
