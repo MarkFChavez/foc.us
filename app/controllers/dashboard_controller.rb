60adeed889b13a06a61975aabadf8d86b72f747e
Mark Chavez <markchav3z@gmail.com>
2018-02-01 07:59:50 +0800

    Eager load action_items and categories.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @notes = current_user.notes.latest
-    @todos = current_user.todos.latest
+    @notes = current_user.notes.latest.includes(:categories)
+    @todos = current_user.todos.latest.includes(:action_items)
   end
 end
